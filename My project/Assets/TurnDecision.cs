acc653a4599f4b82d22bb80a513122afb5ce8e59
VivekPawar04 <112573629+VivekPawar04@users.noreply.github.com>
2022-12-07 01:34:30 +0530

    Turn Based System is ready.
    Now need to add cards

--- My project/Assets/TurnDecision.cs
+++ My project/Assets/TurnDecision.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public enum BattleState { START , PLAYERTURN , PLAYER2TURN }
 public class TurnDecision : MonoBehaviour
@@ -8,6 +11,7 @@ public class TurnDecision : MonoBehaviour
     public Light lt1;
     public Light lt2;
     public BattleState state;
+    public TMP_Text text;
     
     void Start()
     {
@@ -23,18 +27,21 @@ public class TurnDecision : MonoBehaviour
             lt1.enabled = true;
             lt2.enabled = false;
             state = BattleState.PLAYERTURN;
+            text.text = "Player 1";
         }
         else if(state == BattleState.PLAYERTURN)
         {
             lt1.enabled = false;
             lt2.enabled = true;
             state = BattleState.PLAYER2TURN;
+            text.text = "Player 2";
         }
         else if(state == BattleState.PLAYER2TURN)
         {
             state = BattleState.START;
             lt1.enabled = false;
             lt2.enabled = false;
+            text.text = "Start";
         }
     }
 }
